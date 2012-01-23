cd006d7b271bf4c0d0ba0292af80cfccce6144d4
Cam Pedersen <diffference@gmail.com>
2012-01-22 21:45:55 -0500

    [major] mv train to trainOne, add train

--- test/basic.js
+++ test/basic.js
@@ -5,8 +5,7 @@ var net = new neural.Network({
   debug: true
 });
 
-console.log(net.train([1, 2, 3], [0, 1]));
-console.log(net.train([4, 5, 6], [1, 0]));
+console.log(net.trainOne([1, 2, 3], [0, 1]));
 
 console.log(
   net.run([7, 8, 9])
