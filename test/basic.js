96838fdc75a821d15dcc0c343cdb72a14214513f
Cam Pedersen <diffference@gmail.com>
2012-01-22 19:31:06 -0500

    [minor] remove debug

--- test/basic.js
+++ test/basic.js
@@ -1,4 +1,4 @@
-
+var util = require('util');
 var neural = require('../');
 
 var net = new neural.Network({
@@ -10,4 +10,8 @@ console.log(net.train([4, 5, 6], [1, 0]));
 
 console.log(
   net.run([7, 8, 9])
+);
+
+console.log(
+  util.inspect(net, false, null, true)
 );
