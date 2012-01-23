d32c35cbca467371b4b1ced45ed066f429659b85
Cam Pedersen <diffference@gmail.com>
2012-01-22 19:25:01 -0500

    [major] brain dump

--- test/basic.js
+++ test/basic.js
@@ -5,10 +5,9 @@ var net = new neural.Network({
   debug: true
 });
 
-var iterations = 100;
-for (var i = 0; i < iterations; i++) {
-  net.train([1, 2, 3], [0, 1]);
-  net.train([4, 5, 6], [1, 0]);
-}
+console.log(net.train([1, 2, 3], [0, 1]));
+console.log(net.train([4, 5, 6], [1, 0]));
 
-net.run([7, 8, 9]);
+console.log(
+  net.run([7, 8, 9])
+);
